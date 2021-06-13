04cafa7faf51d3ecc4e9ae75a99aa381247bdc94
kerwinzlb <906686343@qq.com>
2021-06-13 21:38:38 +0800

    调整字段instid->instId  On branch main  Changes to be committed: 	modified:   db/mongo.sql

--- db/mongo.sql
+++ db/mongo.sql
@@ -1,4 +1,4 @@
-db.order.aggregate([{$group:{"_id":{"instid":"$instid","side":"$side"},"count":{$sum:1}, "amtSum":{$sum:{$multiply:["$px","$sz"]}}}}])
+db.order.aggregate([{$group:{"_id":{"instId":"$instId","side":"$side"},"count":{$sum:1}, "amtSum":{$sum:{$multiply:["$px","$sz"]}}}}])
 
 db.config.insert({"instId":"ETH-USDT","buyAmt":10,"sellAmt":10,"buyNum":0,"sellNum":0,"buyGridSize":0.005,"sellGridSize":0.005,"gridNum":1,"mode":1,"upperLimit":10000000,"lowerLimit":0,"status":1})
 
